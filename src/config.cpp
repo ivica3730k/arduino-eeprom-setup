017203527f9e5a60e50165190184a7db393e4f53
IcE <ivica-matic@outlook.com>
2020-12-07 02:57:03 +0000

    Added length limit to fields in order not to overfill char buffer

--- src/config.cpp
+++ src/config.cpp
@@ -13,16 +13,41 @@ bool Configuration::write_mqtt_configuration(String mqttServerAddress,
                                              String blue_mqtt_topic)
 {
     config::MQTT_PARAMS conf;
+    if (mqttServerAddress.length() > sizeof(conf.mqttServerAddress))
+    {
+        return false;
+    }
     mqttServerAddress.toCharArray(conf.mqttServerAddress, sizeof(conf.mqttServerAddress));
     conf.useAuthentication = useAuthentication;
+    if (mqttUserName.length() > sizeof(conf.mqttUserName))
+    {
+        return false;
+    }
     mqttUserName.toCharArray(conf.mqttUserName, sizeof(conf.mqttUserName));
+    if (mqttPassword.length() > sizeof(conf.mqttPassword))
+    {
+        return false;
+    }
     mqttPassword.toCharArray(conf.mqttPassword, sizeof(conf.mqttPassword));
+    if (red_mqtt_topic.length() > sizeof(conf.red_mqtt_topic))
+    {
+        return false;
+    }
     red_mqtt_topic.toCharArray(conf.red_mqtt_topic, sizeof(conf.red_mqtt_topic));
+    if (green_mqtt_topic.length() > sizeof(conf.green_mqtt_topic))
+    {
+        return false;
+    }
     green_mqtt_topic.toCharArray(conf.green_mqtt_topic, sizeof(conf.green_mqtt_topic));
+    if (blue_mqtt_topic.length() > sizeof(conf.blue_mqtt_topic))
+    {
+        return false;
+    }
     blue_mqtt_topic.toCharArray(conf.blue_mqtt_topic, sizeof(conf.blue_mqtt_topic));
     char buffer[sizeof(conf)];
     memcpy(&buffer, &conf, sizeof(conf));
     writeEEPROM(sizeof(config::WIFI_DETAILS), buffer, sizeof(buffer));
+    return true;
 }
 
 MQTT_PARAMS Configuration::get_mqtt_configuration()
@@ -40,5 +65,4 @@ MQTT_PARAMS Configuration::get_mqtt_configuration()
     mqtt_parameters.green_mqtt_topic = String(mqtt_parameters_binary.green_mqtt_topic);
     mqtt_parameters.blue_mqtt_topic = String(mqtt_parameters_binary.blue_mqtt_topic);
     return mqtt_parameters;
-
 }
--- test.ino
+++ test.ino
@@ -1,5 +1,6 @@
 #include <AUnit.h>
 #include "src/config.h"
+
 test(eeprom_write_read_mqtt_config)
 {
     Configuration cfg;
@@ -7,7 +8,7 @@ test(eeprom_write_read_mqtt_config)
     in.mqttServerAddress = "Test address";
     in.useAuthentication = true;
     in.mqttUserName = "Test username";
-    in.mqttPassword = "Test passwpord";
+    in.mqttPassword = "Test password";
     in.red_mqtt_topic = "Test red mqtt topic";
     in.green_mqtt_topic = "Test green mqtt topic";
     in.blue_mqtt_topic = "Test blue mqtt topic";
@@ -19,13 +20,157 @@ test(eeprom_write_read_mqtt_config)
                                  in.green_mqtt_topic,
                                  in.blue_mqtt_topic);
     MQTT_PARAMS out = cfg.get_mqtt_configuration();
-    assertEqual(in.mqttServerAddress,out.mqttServerAddress);
-    assertEqual(in.useAuthentication,out.useAuthentication);
-    assertEqual(in.mqttUserName,out.mqttUserName);
-    assertEqual(in.mqttPassword,out.mqttPassword);
-    assertEqual(in.red_mqtt_topic,out.red_mqtt_topic);
-    assertEqual(in.green_mqtt_topic,out.green_mqtt_topic);
-    assertEqual(in.blue_mqtt_topic,out.blue_mqtt_topic);
+    assertEqual(in.mqttServerAddress, out.mqttServerAddress);
+    assertEqual(in.useAuthentication, out.useAuthentication);
+    assertEqual(in.mqttUserName, out.mqttUserName);
+    assertEqual(in.mqttPassword, out.mqttPassword);
+    assertEqual(in.red_mqtt_topic, out.red_mqtt_topic);
+    assertEqual(in.green_mqtt_topic, out.green_mqtt_topic);
+    assertEqual(in.blue_mqtt_topic, out.blue_mqtt_topic);
+}
+
+test(fail_write_mqtt_config_server_address_to_big)
+{
+    Configuration cfg;
+    MQTT_PARAMS in;
+    //in.mqttServerAddress = "Test address";
+    in.mqttServerAddress = "Text  to big this should \
+                        fail lorem ipsum dolor si amet lorem \
+                        ipsum dolor si amet fail this please";
+    in.useAuthentication = true;
+    in.mqttUserName = "Test username";
+    in.mqttPassword = "Test password";
+    in.red_mqtt_topic = "Test red mqtt topic";
+    in.green_mqtt_topic = "Test green mqtt topic";
+    in.blue_mqtt_topic = "Test blue mqtt topic";
+    bool status = cfg.write_mqtt_configuration(in.mqttServerAddress,
+                                               in.useAuthentication,
+                                               in.mqttUserName,
+                                               in.mqttPassword,
+                                               in.red_mqtt_topic,
+                                               in.green_mqtt_topic,
+                                               in.blue_mqtt_topic);
+    assertFalse(status);
+}
+
+test(fail_write_mqtt_config_username_to_big)
+{
+    Configuration cfg;
+    MQTT_PARAMS in;
+    in.mqttServerAddress = "Test address";
+    in.useAuthentication = true;
+    //in.mqttUserName = "Test username";
+    in.mqttUserName = "Text  to big this should \
+                        fail lorem ipsum dolor si amet lorem \
+                        ipsum dolor si amet fail this please";
+    in.mqttPassword = "Test password";
+    in.red_mqtt_topic = "Test red mqtt topic";
+    in.green_mqtt_topic = "Test green mqtt topic";
+    in.blue_mqtt_topic = "Test blue mqtt topic";
+    bool status = cfg.write_mqtt_configuration(in.mqttServerAddress,
+                                               in.useAuthentication,
+                                               in.mqttUserName,
+                                               in.mqttPassword,
+                                               in.red_mqtt_topic,
+                                               in.green_mqtt_topic,
+                                               in.blue_mqtt_topic);
+    assertFalse(status);
+}
+
+test(fail_write_mqtt_config_password_to_big)
+{
+    Configuration cfg;
+    MQTT_PARAMS in;
+    in.mqttServerAddress = "Test address";
+    in.useAuthentication = true;
+    in.mqttUserName = "Test username";
+    //in.mqttPassword = "Test password";
+    in.mqttPassword = "Text  to big this should \
+                        fail lorem ipsum dolor si amet lorem \
+                        ipsum dolor si amet fail this please";
+    in.red_mqtt_topic = "Test red mqtt topic";
+    in.green_mqtt_topic = "Test green mqtt topic";
+    in.blue_mqtt_topic = "Test blue mqtt topic";
+    bool status = cfg.write_mqtt_configuration(in.mqttServerAddress,
+                                               in.useAuthentication,
+                                               in.mqttUserName,
+                                               in.mqttPassword,
+                                               in.red_mqtt_topic,
+                                               in.green_mqtt_topic,
+                                               in.blue_mqtt_topic);
+    assertFalse(status);
+}
+
+test(fail_write_mqtt_config_red_topic_to_big)
+{
+    Configuration cfg;
+    MQTT_PARAMS in;
+    in.mqttServerAddress = "Test address";
+    in.useAuthentication = true;
+    in.mqttUserName = "Test username";
+    in.mqttPassword = "Test password";
+    //in.red_mqtt_topic = "Test red mqtt topic";
+    in.red_mqtt_topic = "Text  to big this should \
+                        fail lorem ipsum dolor si amet lorem \
+                        ipsum dolor si amet fail this please";
+    in.green_mqtt_topic = "Test green mqtt topic";
+    in.blue_mqtt_topic = "Test blue mqtt topic";
+    bool status = cfg.write_mqtt_configuration(in.mqttServerAddress,
+                                               in.useAuthentication,
+                                               in.mqttUserName,
+                                               in.mqttPassword,
+                                               in.red_mqtt_topic,
+                                               in.green_mqtt_topic,
+                                               in.blue_mqtt_topic);
+    assertFalse(status);
+}
+
+test(fail_write_mqtt_config_green_topic_to_big)
+{
+    Configuration cfg;
+    MQTT_PARAMS in;
+    in.mqttServerAddress = "Test address";
+    in.useAuthentication = true;
+    in.mqttUserName = "Test username";
+    in.mqttPassword = "Test password";
+    in.red_mqtt_topic = "Test red mqtt topic";
+    //in.green_mqtt_topic = "Test green mqtt topic";
+    in.green_mqtt_topic = "Text  to big this should \
+                        fail lorem ipsum dolor si amet lorem \
+                        ipsum dolor si amet fail this please";
+    in.blue_mqtt_topic = "Test blue mqtt topic";
+    bool status = cfg.write_mqtt_configuration(in.mqttServerAddress,
+                                               in.useAuthentication,
+                                               in.mqttUserName,
+                                               in.mqttPassword,
+                                               in.red_mqtt_topic,
+                                               in.green_mqtt_topic,
+                                               in.blue_mqtt_topic);
+    assertFalse(status);
+}
+
+test(fail_write_mqtt_config_blue_topic_to_big)
+{
+    Configuration cfg;
+    MQTT_PARAMS in;
+    in.mqttServerAddress = "Test address";
+    in.useAuthentication = true;
+    in.mqttUserName = "Test username";
+    in.mqttPassword = "Test password";
+    in.red_mqtt_topic = "Test red mqtt topic";
+    in.green_mqtt_topic = "Test green mqtt topic";
+    //in.blue_mqtt_topic = "Test blue mqtt topic";
+    in.blue_mqtt_topic = "Text  to big this should \
+                        fail lorem ipsum dolor si amet lorem \
+                        ipsum dolor si amet fail this please";
+    bool status = cfg.write_mqtt_configuration(in.mqttServerAddress,
+                                               in.useAuthentication,
+                                               in.mqttUserName,
+                                               in.mqttPassword,
+                                               in.red_mqtt_topic,
+                                               in.green_mqtt_topic,
+                                               in.blue_mqtt_topic);
+    assertFalse(status);
 }
 
 void setup()
